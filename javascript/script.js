9ef36b94464582430733cc282ed7a12812962154
Koen Pardoen <99033595@mydavinci.nl>
2018-01-18 15:39:08 +0100

    no message

--- javascript/script.js
+++ javascript/script.js
@@ -10,6 +10,14 @@ function startTime() {
     var t  = setTimeout (startTime, 500);
 }
 
+  function time() {
+    if (date('h') == '14') {
+      document.getElementById('txt').innerHTML = "Goedemiddag!"
+    } else {
+
+    }
+  }
+
   function checkTime(i) {
     if (i < 10) {i = "0" + i};
     return i;
